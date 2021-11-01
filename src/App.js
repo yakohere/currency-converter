fa23c04913a834485d6c7f3bf2cdc77532e52290
yakohere <yakhyoismoildjonov@gmail.com>
2021-11-01 21:41:28 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+
+import styled from "styled-components"
+import InputForm from "./InputForm";
+import { ReactComponent as Arrows } from "./assets/arrows.svg"
+import { useEffect, useState } from "react";
+
+const App = () => {
+    const [currencies, setCurrencies] = useState([]);
+    const [fromBase, setFromBase] = useState("USD");
+    const [toBase, setToBase] = useState("UZS");
+    const [rates, setRates] = useState([]);
+    const [from, setFrom] = useState("1");
+
+    useEffect(function () {
+        fetch("https://freecurrencyapi.net/api/v2/latest?apikey=e4c23da0-3b26-11ec-b946-afe9794a701d")
+            .then(response => response.json())
+            .then(data => setCurrencies(Object.keys(data.data)))
+    }, []);
+
+    useEffect(function () {
+        fetch(`https://freecurrencyapi.net/api/v2/latest?apikey=e4c23da0-3b26-11ec-b946-afe9794a701d&base_currency=${fromBase}`)
+            .then(response => response.json())
+            .then(data => setRates(data.data))
+    }, [fromBase])
+
+    return (
+        <Wrapper>
+            <Converter>
+                <InputForm
+                    currencies={["USD", ...currencies]}
+                    title="Menda bor"
+                    currencyClicked={(value) => setFromBase(value)}
+                    active={fromBase}
+                    value={from}
+                    changeValue={(val) => setFrom(val)}
+                />
+
+                <Arrows />
+
+                <InputForm
+                    currencies={["UZS", ...currencies]}
+                    title="Sotib olmoqchiman"
+                    currencyClicked={(value) => setToBase(value)}
+                    active={toBase}
+                    value={(from * rates[toBase]).toFixed(2)}
+                    changeValue={() => { }}
+                />
+            </Converter>
+        </Wrapper>
+    );
+}
+
+export default App;
+
+const Wrapper = styled.div`
+    svg {
+        width: 70px;
+    }
+
+
+`;
+
+const Converter = styled.div`
+    max-width: 900px;
+    min-height: 300px;
+    margin: 100px auto;
+    border-radius: 10px;
+    padding: 50px;
+    box-sizing: border-box;
+    box-shadow: 0px 0px 30px 9px rgba(0, 0, 0, 0.1);
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media(max-width: 900px) {
+        flex-direction: column;
+        margin: 100px;
+    }
+
+    @media(max-width: 700px) {
+        flex-direction: column;
+        margin: 50px;
+    }
+
+    @media(max-width: 550px) {
+        flex-direction: column;
+        margin: 25px;
+    }
+
+    @media(max-width: 500px) {
+        padding: 25px;
+        margin: 15px;
+    }
+`;
---
+++ src/InputForm.js
@@ -0,0 +1,91 @@
+import styled, { css } from "styled-components"
+
+const InputForm = (props) => {
+    return (
+        <Wrapper>
+            <h1>{props.title}</h1>
+
+            <div className="currencies">
+                {
+                    props.currencies.map((e, i) =>
+                        <Currency
+                            active={props.active}
+                            element={e}
+                            key={i}
+                            className="currency"
+                            onClick={() => props.currencyClicked(e)}
+                        >
+                            {e}
+                        </Currency>
+                    )
+                }
+            </div>
+
+            <input type="number" value={props.value} onChange={(e) => props.changeValue(e.target.value)} />
+        </Wrapper>
+    )
+}
+
+export default InputForm;
+
+const Wrapper = styled.div`
+    width: 40%;
+    height: 100%;
+    
+    @media(max-width: 900px) {
+        width: 100%;
+    }
+
+
+    .currencies {
+        width: 100%;
+        height: 50px;
+        display: flex;
+        border: 1px solid #E6E6E6;
+        border-radius: 10px;
+        overflow-x: scroll;
+
+        ::-webkit-scrollbar {
+            display: none;
+        }
+    }
+
+    input {
+        width: 100%;
+        height: 100px;
+        font-size: 50px;
+        margin: 10px 0;
+        border: 1px solid #E6E6E6;
+        border-radius: 10px;
+        outline: 0;
+        padding: 0 0 0 10px;
+        font-weight: bold;
+
+        :focus {
+            border: 1px solid #ffffff;
+            outline: 2px solid #5F77F4;
+        }
+    }
+`;
+
+const Currency = styled.div`
+            width: 60px;
+            height: 50px;
+            padding: 0 10px;
+            font-weight: bold;
+            font-size: 18px;
+            text-align: center;
+            line-height: 50px;
+            border-right: 1px solid #E6E6E6;
+            cursor: pointer;
+
+            ${(props) => css`
+                background-color: ${props.active === props.element ? "#5F77F4" : "#fffff"};
+                color: ${props.active === props.element ? "#ffffff" : "#000000"};
+            `}
+
+
+            :last-child {
+                border: none;
+            }
+`;
